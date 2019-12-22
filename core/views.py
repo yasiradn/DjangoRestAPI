f5718c267677de7f7308796351239301e77e032f
yasir <aff3ad@gmail.com>
2019-12-22 22:04:21 +0200

    GET POST request

--- core/views.py
+++ core/views.py
@@ -3,14 +3,19 @@ from django.http import JsonResponse
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from .models import Post
 
 class TestView(APIView):
     
     def get(self,request, *args, **kwargs):
+        qs = Post.objects.all()
+        serializer = PostSerializer(qs, many=True)
+        return Response(serializer.data)
     
-        data = {
-            'name' : 'fff',
-            'age': 0
-        }
-
-        return Response(data)
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
